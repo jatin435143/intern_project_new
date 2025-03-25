9a1d883b1fa1710c8f490e65486ff46a8d49aba7
jatin435143 <148133643+jatin435143@users.noreply.github.com>
2025-03-25 19:42:03 +0530

    Create sample1.js

---
+++ Sample files/sample1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FileUpload from './FileUpload';
+import ResultDisplay from './ResultDisplay';
+
+function App( {
+    return (
+        <div className="App">
+            <h1>Code Quality Analyzer</h1>
+            <fileUpload />
+        </div
+    );
+}
+export default App;
